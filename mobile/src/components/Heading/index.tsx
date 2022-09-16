eee86bea8f736fce53d5706903b7a84f058c18e4
Rafa Martins <rafamartins.dev@gmail.com>
2022-09-16 10:01:32 -0300

    fix props not beeing passed correctly

--- mobile/src/components/Heading/index.tsx
+++ mobile/src/components/Heading/index.tsx
@@ -8,7 +8,7 @@ interface Props extends ViewProps {
 
 export function Heading({ title, subtitle, ...rest }: Props) {
   return (
-    <View style={styles.container}>
+    <View style={styles.container} {...rest}>
       <Text style={styles.title}>{title}</Text>
       <Text style={styles.subtitle}>{subtitle}</Text>
     </View>
